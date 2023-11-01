9e87beaf04c9b2ea0039d2340e9d0d2d08765cb3
superpanic <superpanic@gmail.com>
2023-11-01 16:31:04 +0100

    basic exoplayer added

--- app/src/main/java/com/superpanic/momaplayer/MainActivity.kt
+++ app/src/main/java/com/superpanic/momaplayer/MainActivity.kt
@@ -1,11 +1,107 @@
 package com.superpanic.momaplayer
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
+import androidx.media3.ui.PlayerView
 
-class MainActivity : AppCompatActivity() {
+@UnstableApi class MainActivity : AppCompatActivity() {
+    private val playbackStateListener: Player.Listener = playbackStateListener()
+    private var player: ExoPlayer? = null
+
+    private var playWhenReady = true
+    private var currentItem = 0
+    private var playbackPosition = 0L
+    private lateinit var video_view : PlayerView
+    private lateinit var text_view : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        video_view = findViewById(R.id.video_view)
+        text_view = findViewById(R.id.text_view)
+    }
+    public override fun onStart() {
+        super.onStart()
+        initializePlayer()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    private fun initializePlayer() {
+        val trackSelector = DefaultTrackSelector(this).apply {
+            setParameters(buildUponParameters().setMaxVideoSizeSd())
+        }
+        player = ExoPlayer.Builder(this)
+            .setTrackSelector(trackSelector)
+            .build()
+            .also {exoPlayer ->
+                video_view.player = exoPlayer
+
+                val mediaItem = MediaItem.Builder()
+                    .setUri(getString(R.string.media_url_dash))
+                    .setMimeType(MimeTypes.APPLICATION_MPD)
+                    .build()
+                exoPlayer.setMediaItem(mediaItem)
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(currentItem, playbackPosition)
+                exoPlayer.addListener(playbackStateListener)
+                exoPlayer.prepare()
+            }
+    }
+
+    private fun releasePlayer() {
+        player?.let { exoPlayer ->
+            playbackPosition = exoPlayer.currentPosition
+            currentItem = exoPlayer.currentMediaItemIndex
+            playWhenReady = exoPlayer.playWhenReady
+            exoPlayer.removeListener(playbackStateListener)
+            exoPlayer.release()
+        }
+        player = null
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, video_view).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+}
+
+private fun playbackStateListener() = object : Player.Listener {
+    override fun onPlaybackStateChagned(playbackState: Int) {
+        val stateString: String = when (playbackState) {
+            ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE"
+            ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING"
+            ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY"
+            ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED"
+            else -> "UNKNOWN_STATE"
+        }
+        Log.d(TAG, "changed state to @stateString")
     }
 }
